b1322d72fc7dd720a54864e26bdee5327a47c743
Argo160 <116899376+Argo160@users.noreply.github.com>
2023-02-12 16:44:12 +0330

    Update st.sh

--- st.sh
+++ st.sh
@@ -9,6 +9,35 @@ if [ -z "$ip" ]
 then
 	echo "IP not entered"
 fi
-echo "Testing speed For $ip : $port"
-speed_download=$(curl --resolve $domain:$port:$ip https://$domain:$port/$file -o /dev/null --connect-timeout 5 --max-time 15 -w %{speed_download} | awk -F\. '{printf ("%d\n",$1/1024)}')
-echo "$ip Average speed $speed_download kB/s"
+
+function speedtesthttps(){
+rm -rf log.txt speed.txt
+curl --resolve $domain:443:$1 https://$domain/$file -o /dev/null --connect-timeout 1 --max-time 10 > log.txt 2>&1
+cat log.txt | tr '\r' '\n' | awk '{print $NF}' | sed '1,3d;$d' | grep -v 'k\|M' >> speed.txt
+for i in `cat log.txt | tr '\r' '\n' | awk '{print $NF}' | sed '1,3d;$d' | grep k | sed 's/k//g'`
+do
+	k=$i
+	k=$[$k*1024]
+	echo $k >> speed.txt
+done
+for i in `cat log.txt | tr '\r' '\n' | awk '{print $NF}' | sed '1,3d;$d' | grep M | sed 's/M//g'`
+do
+	i=$(echo | awk '{print '$i'*10 }')
+	M=$i
+	M=$[$M*1024*1024/10]
+	echo $M >> speed.txt
+done
+max=0
+for i in `cat speed.txt`
+do
+	if [ $i -ge $max ]
+	then
+		max=$i
+	fi
+done
+rm -rf log.txt speed.txt
+echo $max
+}
+max=$(speedtesthttps $ip)
+max=$[$max/1024]
+echo "$ip Best Speed $max kB/s"
