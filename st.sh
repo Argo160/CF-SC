c88bb2f77c51f7d9250177a97e1d98f8ca650ae6
Argo160 <116899376+Argo160@users.noreply.github.com>
2023-02-12 16:09:54 +0330

    Update st.sh

--- st.sh
+++ st.sh
@@ -4,17 +4,11 @@ file=$(echo $url | cut -f 2- -d'/')
 clear
 
 read -p "Enter The IP For Test: " ip
-read -p "Port(Default 443): " port
+port=443
 if [ -z "$ip" ]
 then
 	echo "IP not entered"
 fi
-if [ -z "$port" ]
-then
-	port=443
-fi
 echo "Testing speed For $ip : $port"
 speed_download=$(curl --resolve $domain:$port:$ip https://$domain:$port/$file -o /dev/null --connect-timeout 5 --max-time 15 -w %{speed_download} | awk -F\. '{printf ("%d\n",$1/1024)}')
-}
-
 echo "$ip Average speed $speed_download kB/s"
